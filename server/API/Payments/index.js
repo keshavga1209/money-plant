32970b2ddc37f39d53da2ff3f1956890bafa2c3f
Keshav Gautam <keshavg1209@gmail.com>
2022-02-26 20:59:20 +0530

    payment method

---
+++ server/API/Payments/index.js
@@ -0,0 +1,29 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const paymentRoute = require("./paymentRoute");
+// const mongoose = require("mongoose");
+// const app = express();
+// const path = require("path");
+
+
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// app.use("/api", paymentRoute);
+
+// app.listen(5000, () => {
+//   console.log(`App is running at 5000 port`);
+// });
+
+// //wasted now
+{/* <div class="razorpay-embed-btn" data-url="https://pages.razorpay.com/pl_J0efs16fDfNstS/view" data-text="Donate " data-color="#528FF0" data-size="large">
+  <script>
+    (function(){
+      var d=document; var x=!d.getElementById('razorpay-embed-btn-js')
+      if(x){ var s=d.createElement('script'); s.defer=!0;s.id='razorpay-embed-btn-js';
+      s.src='https://cdn.razorpay.com/static/embed_btn/bundle.js';d.body.appendChild(s);} else{var rzp=window['__rzp__'];
+      rzp && rzp.init && rzp.init()}})();
+  </script>
+</div>
+     */}
